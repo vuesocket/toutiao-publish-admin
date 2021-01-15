78382f5b8d920f27b69eb597d8b890c07e58cf86
vuesocket <vvsocket@outlook.com>
2021-01-15 05:04:12 -0800

    init dir

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>xx信息发布系统</h1>
+
+    <!--路由出口-->
+    <router-view/>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+
+<style lang="less">
+
+</style>
